316da88b0aadd4d1f2e05383d22ed233c9bf939d
Mohamed <babaouimohamedse@gmail.com>
2024-06-24 23:19:00 +0100

    Commited to test auto triggering

--- src/test/java/org/tests/ErrorValidationsTest.java
+++ src/test/java/org/tests/ErrorValidationsTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
-
+//I added this comment just to test how git push would trigger jenkins job
 public class ErrorValidationsTest extends BaseTest {
     ProductsCatalogue productCatalogue;
     CheckoutPage checkoutPage;
